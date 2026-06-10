72a81a9e563b52e6f4daeedfa507bd53c6467074
Maria Elvina <maria.elvina28@gmail.com>
2026-06-10 20:59:17 +0700

    SIAP EAS

--- resources/views/siswa/create.blade.php
+++ resources/views/siswa/create.blade.php
@@ -2,42 +2,64 @@
 @section('title', 'Data Siswa')
 @section('konten')
 
-    <h2>Tambah Siswa</h2>
+    <a href="{{ route('siswa.index') }}" class="btn btn-secondary mb-4">Kembali</a>
 
     @if ($errors->any())
-        <ul style="color: red;">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+        <div class="alert alert-danger">
+            <ul class="mb-0">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
     @endif
 
-    <form action="{{ route('siswa.store') }}" method="POST" onsubmit="return validasiForm()">
-        @csrf
+    <div class="card">
+        <div class="card-header fw-bold">
+            Form Tambah Data Siswa
+        </div>
 
-        <p>
-            <label>NRP</label><br>
-            <input type="text" name="NRP" id="NRP" maxlength="10" value="{{ old('NRP') }}">
-        </p>
+        <div class="card-body">
+            <form action="{{ route('siswa.store') }}" method="POST" onsubmit="return validasiForm()">
+                @csrf
 
-        <p>
-            <label>Nama</label><br>
-            <input type="text" name="Nama" id="Nama" maxlength="20" value="{{ old('Nama') }}">
-        </p>
+                <div class="row mb-3">
+                    <label for="NRP" class="col-sm-2 col-form-label">NRP</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="NRP" id="NRP" class="form-control" maxlength="10" value="{{ old('NRP') }}" placeholder="Masukkan NRP...">
+                    </div>
+                </div>
 
-        <p>
-            <label>Kelas</label><br>
-            <input type="text" name="Kelas" id="Kelas" maxlength="5" value="{{ old('Kelas') }}">
-        </p>
+                <div class="row mb-3">
+                    <label for="Nama" class="col-sm-2 col-form-label">Nama</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="Nama" id="Nama" class="form-control" maxlength="20" value="{{ old('Nama') }}" placeholder="Masukkan nama lengkap...">
+                    </div>
+                </div>
 
-        <p>
-            <label>Tanggal Lahir</label><br>
-            <input type="date" name="TanggalLahir" id="TanggalLahir" value="{{ old('TanggalLahir') }}">
-        </p>
+                <div class="row mb-3">
+                    <label for="Kelas" class="col-sm-2 col-form-label">Kelas</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="Kelas" id="Kelas" class="form-control" maxlength="5" value="{{ old('Kelas') }}" placeholder="Contoh: IF-A">
+                    </div>
+                </div>
 
-        <button type="submit">Simpan</button>
-        <a href="{{ route('siswa.index') }}">Kembali</a>
-    </form>
+                <div class="row mb-3">
+                    <label for="TanggalLahir" class="col-sm-2 col-form-label">Tanggal Lahir</label>
+                    <div class="col-sm-10">
+                        <input type="date" name="TanggalLahir" id="TanggalLahir" class="form-control" value="{{ old('TanggalLahir') }}">
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="offset-sm-2 col-sm-10">
+                        <input type="submit" value="Simpan Data" class="btn btn-primary">
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
 
     <script>
         function validasiForm() {
@@ -107,7 +129,6 @@
                     icon: "error"
                 });
                 return false;
-
             }
 
             return true;
